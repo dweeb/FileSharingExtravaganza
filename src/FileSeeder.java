26570a1751c05bea20ae048ae0d6c44600699abc
dweeb <sadlik512@gmail.com>
2019-01-05 22:59:32 +0100

    adding server-client pair too

---
+++ src/FileSeeder.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.net.Socket;
+
+public class FileSeeder implements Runnable{
+    Socket socket;
+    public FileSeeder(Socket socket){
+        this.socket = socket;
+    }
+    @Override
+    public void run() {
+        //
+    }
+}
